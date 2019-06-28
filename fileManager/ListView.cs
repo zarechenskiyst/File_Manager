e627918dc006b912aa42d6785b2b8ccc66aae543
Stanislav <Zarechenskiyst@gmail.com>
2019-06-28 19:01:22 +0300

    initial commit

---
+++ fileManager/ListView.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fileManager
+{
+    class ListView
+    {
+        private int selectedPrevIndex;
+        private int selectedIndex;
+        private bool wasPainted;
+
+        private int x, y;
+        public List<object> Items { get; set; }
+
+        public ListView(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Clean()
+        {
+            wasPainted = false;
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Console.CursorLeft = x;
+                Console.CursorTop = i+y;
+                Console.Write(new string(' ', Items[i].ToString().Length));
+            }
+        }
+
+        public object Item { get; set; }
+
+        public bool Focused { get; set; }
+
+        public void Render()
+        {
+            for (int i = 0; i < Items.Count; i++)
+            {
+                if (wasPainted)
+                {
+                    if (i != selectedIndex && i != selectedPrevIndex)
+                        continue;
+                }
+
+                var item = Items[i];
+                var saveForeground = Console.ForegroundColor;
+                var saveBackground = Console.BackgroundColor;
+                if (i == selectedIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+
+                Console.CursorLeft = x;
+                Console.CursorTop = i+y;
+                Console.Write(item);
+
+                Console.ForegroundColor = saveForeground;
+                Console.BackgroundColor = saveBackground;
+            }
+            wasPainted = true;
+        }
+        public void Update(ConsoleKeyInfo key)
+        {
+            selectedPrevIndex = selectedIndex;
+            if (key.Key == ConsoleKey.DownArrow && selectedIndex+1<Items.Count)
+                selectedIndex++;
+            else if (key.Key == ConsoleKey.UpArrow && selectedIndex -1 >= 0)
+                selectedIndex--;
+        }
+    }
+}
---
+++ fileManager/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            var view = new ListView();
+            view.Items = new DirectoryInfo(@"C:\").GetFileSystemInfos().Select(f => f.Name).Cast<object>().ToList() ;
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+                view.Update(key);
+                view.Render();
+            }
+        }
+    }
+}
